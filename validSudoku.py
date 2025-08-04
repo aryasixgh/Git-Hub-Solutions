587c6df9b59c0a314ca2902701a648c6cb78a976
aryasixgh <aryasixgh@gmail.com>
2025-08-04 19:36:42 +0530

    Initilaize validSudoku

---
+++ validSudoku.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]):
+        #Check rows
+        #Check cols
+        #Check 3x3
+        return True
+
+def main():
+    print(Solution.isValidSudoku())
+
+if __name__ == "__main__":
+    main()
