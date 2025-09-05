d018fe969026ec294a41305c730adbc318ff87c1
Arya Singh <aryasixgh@gmail.com>
2025-09-05 22:32:13 +0530

    132ms solved

--- Python/Stack/dailyTemp.py
+++ Python/Stack/dailyTemp.py
@@ -1,24 +1,30 @@
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
-        answer = []
-        for i in range(len(temperatures)):
-            tempStack = []
-            for j in range(i, len(temperatures)):
-                if temperatures[j] > temperatures[i]:
-                    tempStack.append(temperatures[j])
-                    break
-                else:
-                    tempStack.append(temperatures[j])
-            if tempStack[0] < tempStack[len(tempStack)-1]:
-               answer.append(len(tempStack)-1)
-            else:
-                answer.append(0)
-            print(tempStack)
+        answer = [0] * len(temperatures)
+        # for i in range(len(temperatures)):
+        #     tempStack = []
+        #     for j in range(i, len(temperatures)):
+        #         if temperatures[j] > temperatures[i]:
+        #             tempStack.append(temperatures[j])
+        #             break
+        #         else:
+        #             tempStack.append(temperatures[j])
+        #     if tempStack[0] < tempStack[len(tempStack)-1]:
+        #        answer.append(len(tempStack)-1)
+        #     else:
+        #         answer.append(0)
+        #     print(tempStack)
+        stack = []
+        for i in range(0, len(temperatures)):
+            while stack and stack[-1][0] < temperatures[i]:
+                numero, indice = stack.pop()
+                answer[indice] = i - indice
+            stack.append((temperatures[i], i))
 
         return answer
 
 def main():
-    temperatures = [30,60,90]
+    temperatures = [73,74,75,71,69,72,76,73]
     print(Solution().dailyTemperatures(temperatures))
 
 if __name__ == "__main__":
