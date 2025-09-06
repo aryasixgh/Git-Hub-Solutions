f9eab48191051c8c974d62ce9841d634dea66a5c
Arya Singh <aryasixgh@gmail.com>
2025-09-06 20:00:02 +0530

    Initialization

---
+++ Python/Stack/carFleet.py
@@ -0,0 +1,9 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        pass
+
+def main():
+    Solution().carFleet()
+
+if __name__ == "__main__":
+    main()
